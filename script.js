6bbe10d95ea88e0c02fb4bf18a350a9bf8cf68e9
elyasbn <elyasbn@gmail.com>
2024-04-01 21:02:10 +0330

    Update 0.0.2 script.js
    مرجع‌ساز فارسی APA به ورژن 0.0.2 آپدیت شد
    
    تغییرات این نسخه: 
    ▫️ذخیره‌سازی منابع ایجادشده و افزودن «فهرست منابع»: حالا می‌توانید به همه ارجاعاتی که ایجاد می‌کنید، دسترسی داشته باشید
    ▫️اضافه‌شدن امکان افزودن چند نویسنده
    ▫️تفکیک ورودی شماره دوره و شماره پیاپی نشریات در بخش «مقاله ژورنال»
    ▫️تفکیک ورودی صفحات آغازین و پایانی مقاله در بخش «مقاله ژورنال»
    ▫️تغییر و بهبود رابط کاربری برنامه
    ▫️رفع بخشی از ایرادات جزئی

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function showCopyNotification() {
     const notification = document.getElementById('copyNotification');
-    notification.classList.remove('hidden'); // Show the notification
+    notification.classList.remove('hidden'); 
 
-    // Hide the notification after 3 seconds
     setTimeout(() => {
         notification.classList.add('hidden');
     }, 3000);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+    displaySavedCitations();
     const copyInTextReferenceBtn = document.getElementById('copyInTextReference');
     const copyFinalReferenceBtn = document.getElementById('copyFinalReference');
 
@@ -33,63 +33,160 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         }
     });
-});
 
-document.getElementById('documentType').addEventListener('change', function() {
-    // Hide all fields initially
-    document.querySelectorAll('div[id$="Fields"]').forEach(function(section) {
-        section.classList.add('hidden');
+    document.getElementById('documentType').addEventListener('change', function() {
+        document.querySelectorAll('div[id$="Fields"]').forEach(function(section) {
+            section.classList.add('hidden');
+        });
+
+        const selectedSection = document.getElementById(`${this.value}Fields`);
+        if (selectedSection) {
+            selectedSection.classList.remove('hidden');
+        }
+
+        const authorsSection = document.getElementById('authorFields');
+        if (this.value === 'book' || this.value === 'journalArticle' || this.value === 'conferencePaper') {
+            authorsSection.classList.remove('hidden');
+        } else {
+            authorsSection.classList.add('hidden');
+        }
     });
 
-    // Show the selected document type fields
-    const selectedSection = document.getElementById(`${this.value}Fields`);
-    if (selectedSection) {
-        selectedSection.classList.remove('hidden');
-    }
+    document.getElementById('addAuthorBtn').addEventListener('click', function() {
+        const authorInputsContainer = document.getElementById('authorInputs');
+        const newAuthorInput = document.createElement('div');
+        newAuthorInput.classList.add('author-input');
+        newAuthorInput.innerHTML = `
+            <input type="text" name="authorFirstName[]" class="mb-2 p-2 border rounded block" placeholder="نام">
+            <input type="text" name="authorLastName[]" class="mb-4 p-2 border rounded block" placeholder="نام خانوادگی">
+        `;
+        authorInputsContainer.appendChild(newAuthorInput);
+    });
+
+    document.getElementById('generateButton').addEventListener('click', generateReference);
 });
 
 function generateReference() {
     const type = document.getElementById('documentType').value;
     let reference = '';
 
-    const authorFirstName = document.querySelector(`#${type}Fields [name="authorFirstName"]`)?.value;
-    const authorLastName = document.querySelector(`#${type}Fields [name="authorLastName"]`)?.value;
+    const authorInputs = document.querySelectorAll(`#${type}Fields [name^="authorFirstName[]"]`);
+    let authors = [];
+    authorInputs.forEach(inputGroup => {
+        const firstName = inputGroup.value.trim();
+        const lastName = inputGroup.nextElementSibling.value.trim();
+        if (firstName !== '' && lastName !== '') {
+            authors.push(`${lastName}، ${firstName}`);
+        }
+    });
+    const authorString = authors.join('; ');
+
     const title = document.querySelector(`#${type}Fields [name="title"]`)?.value || document.querySelector(`#${type}Fields [name="articleTitle"]`)?.value || document.querySelector(`#${type}Fields [name="thesisTitle"]`)?.value || document.querySelector(`#${type}Fields [name="paperTitle"]`)?.value;
     const year = document.querySelector(`#${type}Fields [name="year"]`)?.value;
     const additionalInfo = document.querySelector(`#${type}Fields [name="location"]`)?.value || document.querySelector(`#${type}Fields [name="journalName"]`)?.value || document.querySelector(`#${type}Fields [name="websiteName"]`)?.value || document.querySelector(`#${type}Fields [name="university"]`)?.value || document.querySelector(`#${type}Fields [name="conferenceName"]`)?.value;
     const url = document.querySelector(`#${type}Fields [name="url"]`)?.value;
-    const publishDate = document.querySelector(`#${type}Fields [name="publishDate"]`)?.value;    
+    const publishDate = document.querySelector(`#${type}Fields [name="publishDate"]`)?.value;
 
     switch (type) {
         case 'book':
             const publisher = document.querySelector('#bookFields [name="publisher"]')?.value;
-            reference = `${authorLastName}، ${authorFirstName} (${year}). ${title}. ${additionalInfo}: ${publisher}.`;
+            reference = `${authorString} (${year}). ${title}. ${additionalInfo}: ${publisher}.`;
             break;
-        case 'journalArticle':
-            const volumeIssue = document.querySelector('#journalArticleFields [name="volumeIssue"]')?.value;
-            const pages = document.querySelector('#journalArticleFields [name="pages"]')?.value;
-            reference = `${authorLastName}، ${authorFirstName} (${year}). ${title}. ${additionalInfo}، ${volumeIssue}، ${pages}.`;
-            break;
+            case 'journalArticle':
+                const volume = document.querySelector('#journalArticleFields [name="volume"]')?.value;
+                const issue = document.querySelector('#journalArticleFields [name="issue"]')?.value;
+                const startPage = document.querySelector('#journalArticleFields [name="startPage"]')?.value;
+                const endPage = document.querySelector('#journalArticleFields [name="endPage"]')?.value;
+                let pageRange = '';
+                if (startPage && endPage) {
+                    pageRange = `${startPage}-${endPage}`;
+                } else if (startPage) {
+                    pageRange = startPage;
+                } else if (endPage) {
+                    pageRange = endPage;
+                }
+                reference = `${authorString} (${year}). ${title}. ${additionalInfo}، ${volume}(${issue})، ${pageRange}.`;
+                break;
         case 'website':
             const pageTitle = document.querySelector('#websiteFields [name="pageTitle"]')?.value;
-            reference = `${authorLastName}، ${authorFirstName} (${publishDate}). ${pageTitle}. ${additionalInfo}. بازیابی‌شده از:  ${url}`;
+            reference = `${authorString} (${publishDate}). ${pageTitle}. ${additionalInfo}. بازیابی‌شده از:  ${url}`;
             break;
         case 'dissertation':
             const dissertationType = document.getElementById('dissertationType').value;
             let typeText = dissertationType === 'masterThesis' ? 'پایان‌نامه کارشناسی ارشد' : 'رساله دکتری';
-            reference = `${authorLastName}، ${authorFirstName}. (${year}). ${title}. ${typeText}. ${additionalInfo}.`;
+            reference = `${authorString}. (${year}). ${title}. ${typeText}. ${additionalInfo}.`;
             break;
         case 'conferencePaper':
-            const dateLocation = document.querySelector('#conferencePaperFields [name="date"]')?.value + ', ' + additionalInfo;
-            reference = `${authorLastName}، ${authorFirstName}. (${year}). ${title}. Paper presented at ${additionalInfo}, ${dateLocation}.`;
+            const dateLocation = document.querySelector('#conferencePaperFields [name="date"]')?.value + '، ' + additionalInfo;
+            reference = `${authorString}. (${year}). ${title}. ارائه‌شده در: ${additionalInfo}، ${dateLocation}.`;
             break;
     }
 
-    let inTextReference = `(${authorLastName}، ${year})`;
+    let inTextReference = '';
+
+    const authorLastName = authors.map(item => item.split('،')[0]);
+
+    if (authors.length === 1) {
+        inTextReference = `(${authorLastName[0]}، ${year})`;
+    } else if (authors.length === 2) {
+        inTextReference = `(${authorLastName[0]} و ${authorLastName[1]}، ${year})`;
+    } else if (authors.length > 2) {
+        inTextReference = `(${authorLastName[0]} و همکاران، ${year})`;
+        console.log(authors);
+    }
+
+    saveCitationToLocalStorage(reference);
 
     document.getElementById('finalReferenceOutput').textContent = reference;
     document.getElementById('inTextReferenceOutput').textContent = inTextReference;
     document.getElementById('referenceOutput').classList.remove('hidden');
+
+    displaySavedCitations();
 }
 
-document.getElementById('generateButton').addEventListener('click', generateReference);
+function saveCitationToLocalStorage(citation) {
+    let savedCitations = JSON.parse(localStorage.getItem('savedCitations')) || [];
+    savedCitations.push(citation);
+    localStorage.setItem('savedCitations', JSON.stringify(savedCitations));
+}
+
+function displaySavedCitations() {
+    let savedCitations = JSON.parse(localStorage.getItem('savedCitations')) || [];
+    
+    savedCitations.sort();
+
+    const tableBody = document.getElementById('savedCitationsTableBody');
+    tableBody.innerHTML = '';
+    savedCitations.forEach((citation, index) => {
+        const row = `
+            <tr>
+                <td class="border border-gray-200 px-4 py-2">${citation}</td>
+                <td class="border border-gray-200 px-4 py-2">
+                    <button onclick="copyCitation(${index})" class="px-2 mt-3 py-1 bg-blue-500 text-white rounded w-full">کپی</button>
+                    <button onclick="deleteCitation(${index})" class="px-2 mt-3 py-1 bg-red-500 text-white rounded w-full">حذف</button>
+                </td>
+            </tr>
+        `;
+        tableBody.innerHTML += row;
+    });
+}
+
+function copyCitation(index) {
+    const savedCitations = JSON.parse(localStorage.getItem('savedCitations')) || [];
+    const citation = savedCitations[index];
+    if (citation) {
+        navigator.clipboard.writeText(citation).then(() => {
+            alert('کپی شد!');
+        }).catch(err => {
+            console.error('خطا در هنگام کپی: ', err);
+        });
+    }
+}
+
+function deleteCitation(index) {
+    let savedCitations = JSON.parse(localStorage.getItem('savedCitations')) || [];
+    savedCitations.splice(index, 1);
+    localStorage.setItem('savedCitations', JSON.stringify(savedCitations));
+    displaySavedCitations();
+}
+
